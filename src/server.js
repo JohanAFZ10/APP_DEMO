6fb40123ba87aa73a481695af463d7b28fd06be7
Johan Fernandez <info.ottishardware@gmail.com>
2022-03-14 13:28:55 -0500

    APP_DEMO V0.0 - Creamos el servidor y logramos devolver una Pagina a traves de una peticion Get

--- src/server.js
+++ src/server.js
@@ -1,15 +1,34 @@
-const express = require('express')
-const app = express()
+// Archivo del Servidor HTTP
+// Incluimos el paquete HTTP
+const http = require('http');
+// Incluimos el paquete express
+const express = require('express');
+// Creamos una App de Express
+const app = express();
+// Declaramos el puerto del Servidor
+const port = 3000;
+// Incluimos el paquete path
+const path = require('path');
+// Incluir Morgan (Seguimiento de las Peticiones HTTP)
+//const morgan = require('morgan');
+// Enlazamos la App con un servidor HTTP
+const server = http.createServer(app);
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
 
-app.get('/hola',function (req, res){
-    res.send('hola soy el servidor')
+/******************************************************/
+app.use(express.urlencoded({extended:false}));
+// Esto le permite al servidor recibir o enviar .json
+app.use(express.json());
+// Configurar los archivos estáticos del servidor
+app.use(express.static('src'));
+/*------------------------------------------------------*/
+
+app.get('/login', function(req, res){
+  res.sendFile(path.resolve(__dirname,'./frontend/app.html'))
 });
-app.get('/test',function (req, res){
-    res.send('inicie con el test')
-});
 
-app.listen(3000)
+// Inicializar el servidor
+server.listen(port, ()=>{
+  console.log("Server on: http://localhost:"+port);
+ // console.log(path.resolve(__dirname,'./frontend/app.html'));
+});
